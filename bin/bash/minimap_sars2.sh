63bb15deff642e37c4a82e98353d9bf4a6688c03
jnoms <jasonlnomburg@gmail.com>
2020-06-11 16:06:12 -0400

    Added functionality to map short-read illumina data directly from this script.

--- bin/bash/minimap_sars2.sh
+++ bin/bash/minimap_sars2.sh
@@ -25,6 +25,15 @@ usage() {
         Optional:
         -t <THREADS> [1]
             Number of computing threads available.
+        -m <MODE> [long]
+            Options: 'long', 'short'
+            - long:
+              Maps input long (nanopore) reads using settings optimized
+              for a small viral reference like SARS-Cov-2
+            - short:
+              Maps input *short reads* against a desired target. This simply
+              engages the sr (genomic short-read mapping) preset. No other
+              mapping settings are changed.
         "
 }
 
@@ -35,13 +44,14 @@ if [ $# -le 3 ] ; then
 fi
 
 #Setting input
-while getopts i:r:o:t: option ; do
+while getopts i:r:o:t:m: option ; do
         case "${option}"
         in
                 i) INPUT_FASTX=${OPTARG};;
                 r) REF_FASTA=${OPTARG};;
                 o) OUT_BAM=${OPTARG};;
                 t) THREADS=${OPTARG};;
+                m) MODE=${OPTARG};;
         esac
 done
 
@@ -49,6 +59,7 @@ done
 # Set defaults
 #------------------------------------------------------------------------------#
 THREADS=${THREADS:-1}
+MODE=${MODE:-long}
 
 #------------------------------------------------------------------------------#
 # Run cd-hit
@@ -60,6 +71,7 @@ INPUT_FASTX: $INPUT_FASTX
 REF_FASTA: $REF_FASTA
 OUT_BAM: $OUT_BAM
 THREADS: $THREADS
+MODE: $MODE
 "
 
 
@@ -69,27 +81,42 @@ echo "Starting script."
 mkdir -p $(dirname $OUT_BAM)
 
 # Run minimap
-minimap2 \
--a \
--x splice \
--t $THREADS \
---sam-hit-only \
--k 8 \
--w 1 \
---splice \
--g 30000 \
--G 30000 \
--A1 \
--B2 \
--O2,24 \
--E1,0 \
---no-end-flt \
--F 40000 \
--N 32 \
---splice-flank=no \
---max-chain-skip=40 \
--p 0.7 \
-$REF_FASTA $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
-samtools index $OUT_BAM
+if [[ $MODE == "long" ]] ; then
+  minimap2 \
+  -a \
+  -x splice \
+  -t $THREADS \
+  --sam-hit-only \
+  -k 8 \
+  -w 1 \
+  --splice \
+  -g 30000 \
+  -G 30000 \
+  -A1 \
+  -B2 \
+  -O2,24 \
+  -E1,0 \
+  --no-end-flt \
+  -F 40000 \
+  -N 32 \
+  --splice-flank=no \
+  --max-chain-skip=40 \
+  -p 0.7 \
+  $REF_FASTA $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
+  samtools index $OUT_BAM
+
+elif [[ $MODE == "short" ]] ; then
+  minimap2 \
+  -t $THREADS \
+  -a \
+  -x sr \
+  --sam-hit-only \
+  $REF_FASTA \
+  $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
+  samtools index $OUT_BAM
+else
+  echo "MODE must be set to 'long' or 'short'. You specified $MODE"
+  exit 1
+fi
 
 echo "Finished."
