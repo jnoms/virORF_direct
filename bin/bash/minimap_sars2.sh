158a84930827c4121a64df30bed8ddccaab09716
jnoms <jasonlnomburg@gmail.com>
2020-06-12 12:02:03 -0400

    Revert minimap script. I found that the short mode is not very effective at identifying junctions. Use the minimap settings that are in the script currently - as long as the short reads are long enough, it still finds junctions. 75bp reads are insufficient.

--- bin/bash/minimap_sars2.sh
+++ bin/bash/minimap_sars2.sh
@@ -25,15 +25,6 @@ usage() {
         Optional:
         -t <THREADS> [1]
             Number of computing threads available.
-        -m <MODE> [long]
-            Options: 'long', 'short'
-            - long:
-              Maps input long (nanopore) reads using settings optimized
-              for a small viral reference like SARS-Cov-2
-            - short:
-              Maps input *short reads* against a desired target. This simply
-              engages the sr (genomic short-read mapping) preset. No other
-              mapping settings are changed.
         "
 }
 
@@ -44,14 +35,13 @@ if [ $# -le 3 ] ; then
 fi
 
 #Setting input
-while getopts i:r:o:t:m: option ; do
+while getopts i:r:o:t: option ; do
         case "${option}"
         in
                 i) INPUT_FASTX=${OPTARG};;
                 r) REF_FASTA=${OPTARG};;
                 o) OUT_BAM=${OPTARG};;
                 t) THREADS=${OPTARG};;
-                m) MODE=${OPTARG};;
         esac
 done
 
@@ -59,7 +49,6 @@ done
 # Set defaults
 #------------------------------------------------------------------------------#
 THREADS=${THREADS:-1}
-MODE=${MODE:-long}
 
 #------------------------------------------------------------------------------#
 # Run cd-hit
@@ -71,7 +60,6 @@ INPUT_FASTX: $INPUT_FASTX
 REF_FASTA: $REF_FASTA
 OUT_BAM: $OUT_BAM
 THREADS: $THREADS
-MODE: $MODE
 "
 
 
@@ -81,42 +69,27 @@ echo "Starting script."
 mkdir -p $(dirname $OUT_BAM)
 
 # Run minimap
-if [[ $MODE == "long" ]] ; then
-  minimap2 \
-  -a \
-  -x splice \
-  -t $THREADS \
-  --sam-hit-only \
-  -k 8 \
-  -w 1 \
-  --splice \
-  -g 30000 \
-  -G 30000 \
-  -A1 \
-  -B2 \
-  -O2,24 \
-  -E1,0 \
-  --no-end-flt \
-  -F 40000 \
-  -N 32 \
-  --splice-flank=no \
-  --max-chain-skip=40 \
-  -p 0.7 \
-  $REF_FASTA $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
-  samtools index $OUT_BAM
-
-elif [[ $MODE == "short" ]] ; then
-  minimap2 \
-  -t $THREADS \
-  -a \
-  -x sr \
-  --sam-hit-only \
-  $REF_FASTA \
-  $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
-  samtools index $OUT_BAM
-else
-  echo "MODE must be set to 'long' or 'short'. You specified $MODE"
-  exit 1
-fi
+minimap2 \
+-a \
+-x splice \
+-t $THREADS \
+--sam-hit-only \
+-k 8 \
+-w 1 \
+--splice \
+-g 30000 \
+-G 30000 \
+-A1 \
+-B2 \
+-O2,24 \
+-E1,0 \
+--no-end-flt \
+-F 40000 \
+-N 32 \
+--splice-flank=no \
+--max-chain-skip=40 \
+-p 0.7 \
+$REF_FASTA $INPUT_FASTX | samtools view -bh -@ $THREADS | samtools sort -@ $THREADS > $OUT_BAM
+samtools index $OUT_BAM
 
 echo "Finished."
